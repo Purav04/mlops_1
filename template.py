8327db636c5f46b00721e50851bead6294508660
Purav04 <puravpatel408@gmail.com>
2022-02-17 17:49:14 +0530

    first commmit

---
+++ template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 16:13:11 2022
+
+@author: purav
+"""
+
+import os
+
+dirs = [
+       os.path.join("data","raw"),
+       os.path.join("data","processed"),
+       "notebooks",
+       "saved_models",
+       "src"
+       ]
+
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
+        pass
+
+files = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src","__init__.py"),
+    "README.md"
+    ]
+
+for file_ in files:
+    with open(file_, "x") as f:
+        pass
